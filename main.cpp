cf36e9737eba79bf5b7fabf5b213c1590f4aa140
ALE <aleronpower@hotmail.com>
2019-10-26 20:10:28 -0300

    Casi tengo completa la idea del main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 #include "allegro5/allegro_primitives.h"
 #include "imgui.h"
 #include "imgui_impl_allegro5.h"
+
+static void init_interface(ALLEGRO_DISPLAY*& display, ALLEGRO_EVENT_QUEUE*& queue);
+static void close_interface(ALLEGRO_DISPLAY*& display, ALLEGRO_EVENT_QUEUE*& queue);
+
+static void start_frame(ALLEGRO_DISPLAY*& display, ALLEGRO_EVENT_QUEUE*& queue,bool& running);
+static void end_frame(ImVec4 color);
+
+
 void alexTesteaImGui(void);
 #define SCREEN_WIDTH	1280
 #define	SCREEN_HEIGHT	720
@@ -12,15 +20,41 @@ int main() {
 }
 
 void alexTesteaImGui(void) {
+
+	ALLEGRO_DISPLAY* display;
+	ALLEGRO_EVENT_QUEUE* queue;
+
+	init_interface(display, queue);
+
+	ImVec4 backColor = ImVec4(0.f, 0.75f, 0.75f, 1.f);
+
+	bool running = true;
+	while (running) {
+		start_frame(display, queue, running);
+
+
+		ImGui::Begin("Oh yeah Mr. Crabs!",&running);
+		ImGui::Text("Nyan!");
+		ImGui::End();
+
+		end_frame(backColor);
+	}
+	close_interface(display, queue);
+}
+
+
+
+
+static void init_interface(ALLEGRO_DISPLAY*& display, ALLEGRO_EVENT_QUEUE*& queue) {
 	//Inicializa Allegro
 	al_init();
 	al_install_keyboard();
 	al_install_mouse();
 	al_init_primitives_addon();
 	al_set_new_display_flags(ALLEGRO_RESIZABLE);
-	ALLEGRO_DISPLAY* display = al_create_display(SCREEN_WIDTH, SCREEN_HEIGHT);
+	display = al_create_display(SCREEN_WIDTH, SCREEN_HEIGHT);
 	al_set_window_title(display, "TP FINAL PARTE 1");
-	ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+	queue = al_create_event_queue();
 	al_register_event_source(queue, al_get_display_event_source(display));
 	al_register_event_source(queue, al_get_keyboard_event_source());
 	al_register_event_source(queue, al_get_mouse_event_source());
@@ -33,45 +67,36 @@ void alexTesteaImGui(void) {
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 
 	ImGui::StyleColorsDark();
-
 	ImGui_ImplAllegro5_Init(display);
-
-	ImGui_ImplAllegro5_NewFrame();
-
-	ImVec4 backColor = ImVec4(0.f, 0.75f, 0.75f, 1.f);
-	bool running = true;
-
-	while (running) {
-		ALLEGRO_EVENT ev;
-		while (al_get_next_event(queue, &ev)) {
-			ImGui_ImplAllegro5_ProcessEvent(&ev);
-			if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
-				running = false;
-			if (ev.type == ALLEGRO_EVENT_DISPLAY_RESIZE) {
-				ImGui_ImplAllegro5_InvalidateDeviceObjects();
-				al_acknowledge_resize(display);
-				ImGui_ImplAllegro5_CreateDeviceObjects();
-			}
-		}
-
-		ImGui_ImplAllegro5_NewFrame();
-		ImGui::NewFrame();
-
-		ImGui::Begin("Oh yeah Mr. Crabs!",&running);
-		ImGui::Text("Nyan!");
-		ImGui::End();
-
-		ImGui::Render();
-		al_clear_to_color(al_map_rgba_f(backColor.x, backColor.y, backColor.z, backColor.w));
-		ImGui_ImplAllegro5_RenderDrawData(ImGui::GetDrawData());
-		al_flip_display();
-	}
-
+}
+static void close_interface(ALLEGRO_DISPLAY*& display, ALLEGRO_EVENT_QUEUE*& queue) {
 	ImGui_ImplAllegro5_Shutdown();
 	ImGui::DestroyContext();
 	al_destroy_event_queue(queue);
 	al_destroy_display(display);
-
-
 }
 
+
+static void start_frame(ALLEGRO_DISPLAY*& display, ALLEGRO_EVENT_QUEUE*& queue, bool& running) {
+	ALLEGRO_EVENT ev;
+	while (al_get_next_event(queue, &ev)) {
+		ImGui_ImplAllegro5_ProcessEvent(&ev);
+		if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+			running = false;
+		if (ev.type == ALLEGRO_EVENT_DISPLAY_RESIZE) {
+			ImGui_ImplAllegro5_InvalidateDeviceObjects();
+			al_acknowledge_resize(display);
+			ImGui_ImplAllegro5_CreateDeviceObjects();
+		}
+	}
+
+	ImGui_ImplAllegro5_NewFrame();
+	ImGui::NewFrame();
+}
+
+static void end_frame(ImVec4 color) {
+	ImGui::Render();
+	al_clear_to_color(al_map_rgba_f(color.x, color.y, color.z, color.w));
+	ImGui_ImplAllegro5_RenderDrawData(ImGui::GetDrawData());
+	al_flip_display();
+}
