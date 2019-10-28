90c61264ec1f92056e3b18a0a4cf372ca606ceb9
ALE <aleronpower@hotmail.com>
2019-10-28 00:08:19 -0300

    Viewer practicamente listo

--- alex_test_trees.cpp
+++ alex_test_trees.cpp
@@ -73,13 +73,13 @@ void initAlex(void) {
 	a[i].nTx = 4;
 
 	i = 7;
-	a[i].tree = { "A","B","C","D","AB","CB","ABCD" };
+	a[i].tree = { "A","B","C","D","E","F","G","H","I","I","I","I","I","I","I","I","AB","CB","EF","GH","II","II","II","II","ABCD","EFGH","IIII","IIII","ABCDEFGH","IIIIIIII","ABCDEFGHIIIIIIII" };
 	a[i].height = 8;
 	a[i].nonce = 1234;
 	a[i].blockId = "BLABLA";
 	a[i].previousBlockId = "----";
 	a[i].merkleRoot = "ABCD";
-	a[i].nTx = 4;
+	a[i].nTx = 9;
 
 	i = 8;
 	a[i].tree = { "A","B","C","D","AB","CB","ABCD" };
--- treeWindow.cpp
+++ treeWindow.cpp
@@ -2,6 +2,9 @@
 
 #include "imgui.h"
 
+#define verticalStep 25.
+#define horizontalStep 25.
+
 treeWindow::treeWindow(const MerkelTree& t) :
 	tree(t), open(true), validRoot(false)
 {
@@ -20,15 +23,19 @@ void treeWindow::replace(const MerkelTree& t)
 }
 
 void treeWindow::draw(void) {
-	ImGui::Begin(windowName.c_str(),&open);
-	ImGui::Text("Numero de bloque: %lu",tree.height);
-	ImGui::Text("ID del bloque: %s",tree.blockId.c_str());
-	ImGui::Text("ID del bloque anterior: %s", tree.previousBlockId.c_str());
-	ImGui::Text("Numero de transaccioens: %lu", tree.nTx);
-	ImGui::Text("Nonce: %lu", tree.nonce);
+	ImGui::Begin(windowName.c_str(), &open);
 
 	if (validRoot == false) {
 		ImGui::Text("PRECAUCION! El MerkleRoot miembro del bloque, y el calculado a partir de las transacciones, no coinciden");
+	}
+
+	if (ImGui::TreeNode("Data")) {
+		ImGui::Text("Numero de bloque: %lu", tree.height);
+		ImGui::Text("ID del bloque: %s", tree.blockId.c_str());
+		ImGui::Text("ID del bloque anterior: %s", tree.previousBlockId.c_str());
+		ImGui::Text("Numero de transaccioens: %lu", tree.nTx);
+		ImGui::Text("Nonce: %lu", tree.nonce);
+		ImGui::TreePop();
 	}
 
 	drawTree();
@@ -56,6 +63,18 @@ void treeWindow::drawTree(void)
 	if (ImGui::TreeNode("Merkle Tree")) {
 		ImGui::BeginChild("child", ImVec2(0, 0), true, ImGuiWindowFlags_HorizontalScrollbar);
 		{
+			longN leafs = tree.nTx;
+			longN height = (longN)ceil(log2(leafs)) + 1;
+			longN width = 1 << (height - 1);
+			longN floor;
+			vector<newIDstr>::iterator it = tree.tree.begin();
+
+			for (floor = height; floor > 0; floor--) {
+				drawFloor(height, floor, leafs,it);
+			}
+
+
+			/*
 			static bool f = false;
 			static bool t = true;
 			ImGui::SetCursorPos(ImVec2(20,20));
@@ -65,9 +84,37 @@ void treeWindow::drawTree(void)
 
 			f = false;
 			t = true;
+			*/
 		}
 		ImGui::EndChild();
 		ImGui::TreePop();
 	}
 	//TO DO!!!!
+}
+
+void treeWindow::drawFloor(const longN totalHeight, const longN floor, const longN leafs, vector<newIDstr>::iterator& it)
+{
+	longN nodes = 1 << (floor - 1);
+	double offset = exp2((signed long)(totalHeight-floor-1)) * horizontalStep;
+	double distance = 2 * offset;
+	double verPos = verticalStep * floor;
+	double horPos = offset;
+	bool marked = false;
+	for (longN i = 0; i < nodes; i++, it++, horPos+=distance) {
+		if (floor != totalHeight)
+			marked = false;
+		else {
+			if (i < leafs) {
+				marked = true;
+			}
+			else {
+				marked = false;
+			}
+		}
+		ImGui::SetCursorPos(ImVec2(horPos, verPos));
+		ImGui::Checkbox("", &marked);
+		if (ImGui::IsItemHovered()) {
+			ImGui::SetTooltip("Merkel: %s",it->c_str());
+		}
+	}
 }
